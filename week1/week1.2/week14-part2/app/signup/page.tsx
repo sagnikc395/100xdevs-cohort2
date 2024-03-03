9f663b0bd1b6d68d3c194de3a3aa7735ec1f2711
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-03-03 19:59:27 +0530

    src: ugly signup

---
+++ week1/week1.2/week14-part2/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+  return (
+    <div>
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+      <button>SignUp</button>
+    </div>
+  );
+}
