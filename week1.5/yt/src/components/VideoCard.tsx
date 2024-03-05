bb6546e413b51e2ee89131167488195ad3d21c08
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-03-05 08:43:50 +0530

    src: basic thumbnail

--- week1.5/yt/src/components/VideoCard.tsx
+++ week1.5/yt/src/components/VideoCard.tsx
@@ -1,20 +1,29 @@
-export function VideoCardComponent() {
+type VideoCardProps = {
+  thumbnail: string;
+  profilepic: string;
+  title: string;
+  author: string;
+  views: string;
+  timestamp: string;
+};
+
+export function VideoCardComponent(props: VideoCardProps) {
   return (
     <div>
-      <img src="./photo2.png" className="rounded-xl"></img>
+      <img src={props.thumbnail} className="rounded-xl"></img>
       <div className="grid grid-cols-12">
         <div className="col-span-1">
           <img
             className="rounded-2xl w-20 h-20 p-2"
-            src="./profilepic.png"
+            src={props.profilepic}
           ></img>
         </div>
         <div className="col-span-11 pt-5">
-          <div className="pl-5 text-slate-100">Nietzsche</div>
-          <div className="pl-5 text-base text-slate-100">HorsesonYT</div>
+          <div className="pl-5 text-slate-100">{props.title}</div>
+          <div className="pl-5 text-base text-slate-100">{props.author}</div>
         </div>
         <div className="col-span-11  text-slate-100 text-base">
-          8 Mn | 5 Days Ago
+          {props.views} | {props.timestamp}
         </div>
       </div>
     </div>
--- week1.5/yt/src/pages/index.tsx
+++ week1.5/yt/src/pages/index.tsx
@@ -8,7 +8,15 @@ export default function Home() {
     <div className={inter.className}>
       <main className="bg-slate-700 h-screen">
         <h2 className="text-slate-100 font-xl">yt-clone</h2>
-        <VideoCardComponent />
+
+        <VideoCardComponent
+          thumbnail={"photo2.jpg"}
+          profilepic={"profilepic.jpg"}
+          title={"Nietzsche"}
+          author={"HorsesOnYT"}
+          views={"8Mn"}
+          timestamp={`${new Date().getDate()}`}
+        />
       </main>
     </div>
   );
