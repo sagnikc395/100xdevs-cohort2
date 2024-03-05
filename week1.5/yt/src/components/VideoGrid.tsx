dd14c23332aac3c11b0832d606c3b68e9c913bca
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-03-05 23:02:10 +0530

    src: made the view responsive

--- week1.5/yt/src/components/VideoGrid.tsx
+++ week1.5/yt/src/components/VideoGrid.tsx
@@ -26,11 +26,20 @@ const VIDEOS = [
     views: "100k",
     timestamp: "2 days to go",
   },
+
+  {
+    title: "How to learn coding4 in 30 days | 30 days plan | Code with me",
+    profilepic: profilephoto,
+    thumbnail: thumbnail,
+    author: "sagnik",
+    views: "100k",
+    timestamp: "2 days to go",
+  },
 ];
 
 export function VideoGrid() {
   return (
-    <div className="grid grid-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
       {VIDEOS.map((video) => {
         return (
           <div>
--- week1.5/yt/src/pages/index.tsx
+++ week1.5/yt/src/pages/index.tsx
@@ -1,24 +1,15 @@
 import { Inter } from "next/font/google";
-import { VideoCardComponent } from "@/components/VideoCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
-import thumbnail from "../../public/photo2.png";
-import profilepic from "../../public/profilepic.png";
+import { VideoGrid } from "@/components/VideoGrid";
 
 export default function Home() {
   return (
     <div className={inter.className}>
       <main className="bg-slate-700 h-screen">
         <h2 className="text-slate-100 font-xl">yt-clone</h2>
-        <VideoCardComponent
-          thumbnail={thumbnail}
-          profilepic={profilepic}
-          title={"Nietzsche"}
-          author={"HorsesOnYT"}
-          views={"8Mn"}
-          timestamp={`${new Date().getDate()}`}
-        />
+        <VideoGrid />
       </main>
     </div>
   );
