8dd2025542d9b11e457b9424107a949135dab6ef
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-03-05 08:37:35 +0530

    ui: basic ui of the yt card done

--- week1.5/yt/src/components/VideoCard.tsx
+++ week1.5/yt/src/components/VideoCard.tsx
@@ -1,10 +1,22 @@
-//here
-
 export function VideoCardComponent() {
   return (
     <div>
-      Hi from video card html component
-      <img src="./photo.png" />
+      <img src="./photo2.png" className="rounded-xl"></img>
+      <div className="grid grid-cols-12">
+        <div className="col-span-1">
+          <img
+            className="rounded-2xl w-20 h-20 p-2"
+            src="./profilepic.png"
+          ></img>
+        </div>
+        <div className="col-span-11 pt-5">
+          <div className="pl-5 text-slate-100">Nietzsche</div>
+          <div className="pl-5 text-base text-slate-100">HorsesonYT</div>
+        </div>
+        <div className="col-span-11  text-slate-100 text-base">
+          8 Mn | 5 Days Ago
+        </div>
+      </div>
     </div>
   );
 }
--- week1.5/yt/src/pages/index.tsx
+++ week1.5/yt/src/pages/index.tsx
@@ -4,8 +4,12 @@ import { VideoCardComponent } from "@/components/VideoCard";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className={inter.className}>
-    <h2>yt-clone</h2>
-    <VideoCardComponent />
-  </div>;
+  return (
+    <div className={inter.className}>
+      <main className="bg-slate-700 h-screen">
+        <h2 className="text-slate-100 font-xl">yt-clone</h2>
+        <VideoCardComponent />
+      </main>
+    </div>
+  );
 }
